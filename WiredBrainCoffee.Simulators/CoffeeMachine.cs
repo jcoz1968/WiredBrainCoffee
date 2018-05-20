c554e9c97e0a11b408625d454f2da95a03320f9b
Jeffery Cosby <jcoz1968@hotmail.com>
2018-05-20 17:42:55 -0500

    add save state project

--- WiredBrainCoffee.Simulators/CoffeeMachine.cs
+++ WiredBrainCoffee.Simulators/CoffeeMachine.cs
@@ -1,15 +1,29 @@
 ﻿using System;
+using WiredBrainCoffee.Storage;
 
 namespace WiredBrainCoffee.Simulators
 {
     public class CoffeeMachine
     {
+        private CoffeeMachineStateSaver _coffeeMachineStateSaver;
+
         public int CounterCappuccino { get; private set; }
+
+        public CoffeeMachine()
+        {
+            _coffeeMachineStateSaver = new CoffeeMachineStateSaver();
+            var state = _coffeeMachineStateSaver.Load();
+            CounterCappuccino = state.CounterCappuccino;
+        }
 
         public void MakeCappuccino()
         {
             CounterCappuccino ++;
             Console.WriteLine($"Make cappuccino number {CounterCappuccino}");
+            _coffeeMachineStateSaver.Save(new CoffeeMachineState
+            {
+                CounterCappuccino = CounterCappuccino
+            });
         }
     }
 }
---
+++ WiredBrainCoffee.Storage/CoffeeMachineStateSaver.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WiredBrainCoffee.Storage
+{
+    public class CoffeeMachineStateSaver
+    {
+        private string _filename = "machine.json";
+
+        public void Save(CoffeeMachineState state)
+        {
+            var json = JsonConvert.SerializeObject(state);
+            File.WriteAllText(_filename, json);
+        }
+
+        public CoffeeMachineState Load()
+        {
+            if(File.Exists(_filename))
+            {
+                var json = File.ReadAllText(_filename);
+                return JsonConvert.DeserializeObject<CoffeeMachineState>(json);
+            }
+            return new CoffeeMachineState();
+        }
+
+        public void ShowStoredJson()
+        {
+
+        }
+    }
+}
